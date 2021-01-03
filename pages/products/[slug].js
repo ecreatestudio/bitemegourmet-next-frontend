321d54f654c23721df9288a9b72adafbbdf643d3
Scott Thomson <scott@ecreate.studio>
2021-01-03 11:37:52 +1100

    Add qty input field plus snipcart data field

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -2,12 +2,17 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { getProducts, getProduct } from "../../utils/api";
 import { getStrapiMedia } from "../../utils/medias";
+import { useState } from 'react';
 
 const ProductPage = ({ product }) => {
   const router = useRouter();
   if (router.isFallback) {
     return <div>Loading category...</div>;
   }
+  const [qtyValue, setQtyValue] = useState(1);
+  const onQtyChange = (event) => {
+    setQtyValue(event.target.value);
+  };
 
   return (
     <div className="m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 mt-8">
@@ -27,21 +32,33 @@ const ProductPage = ({ product }) => {
             {product.title}
           </h4>
           <div className="mt-1 font-semibold text-lg">${product.price} <span className="font-light">{product.serving}</span></div>
+          
             {product.status === "published" ? (
-                <button
-                  className="snipcart-add-item w-full mt-4 bg-white border border-gray-200 d hover:shadow-lg text-gray-700 font-semibold py-2 px-4 rounded shadow"
-                  data-item-id={product.id}
-                  data-item-price={product.price}
-                  data-item-url={router.asPath}
-                  data-item-description={product.description}
-                  data-item-image={getStrapiMedia(
-                    product.image.formats.thumbnail.url
-                  )}
-                  data-item-name={product.title}
-                  v-bind="customFields"
-                >
-                  Add to cart
-                </button>
+              <div className="flex flex-wrap">
+                <div className="flex-initial">
+                  <div className="mt-4 text-gray-700 font-semibold pr-4">
+                    <label>Qty:</label> <input value={qtyValue} onChange={onQtyChange} id="quantity" type="number" 
+                      className="text-center py-2" style={{maxWidth: 5 + 'em'}}></input>
+                  </div>
+                </div>
+                <div className="flex-auto">
+                  <button
+                    className="snipcart-add-item w-full mt-4 bg-white border border-gray-200 d hover:shadow-lg text-gray-700 font-semibold py-2 px-6 rounded shadow"
+                    data-item-id={product.id}
+                    data-item-price={product.price}
+                    data-item-url={router.asPath}
+                    data-item-description={product.description}
+                    data-item-image={getStrapiMedia(
+                      product.image.formats.thumbnail.url
+                    )}
+                    data-item-name={product.title}
+                    data-item-quantity={qtyValue}
+                    v-bind="customFields"
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
           ) : (
             <div className="text-center mr-10 mb-1" v-else>
               <div
