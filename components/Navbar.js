2b142dedc77b24a5a9da12b23e9660fcd3faec8c
Scott Thomson <scott@ecreate.studio>
2021-02-03 22:53:59 +1100

    Always show menu on desktop, tune flex direction

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,18 +9,18 @@ const Navbar = ({ categories = [] }) => {
   const [menuVisible, setMenuVisible] = useState('hidden');
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => {setMenuOpen(!menuOpen)};
-  useEffect(() => {setMenuVisible(menuOpen ? 'block': 'hidden')},[menuOpen]);
+  useEffect(() => {setMenuVisible(menuOpen ? 'block': 'hidden lg:block')},[menuOpen]);
 
   return (
-    <div className="flex flex-col bg-gray-800 text-gray-300 min-w-1/4 rounded-lg lg:my-6">
+    <div className="flex flex-col bg-gray-800 text-gray-300 min-w-1/4 rounded-lg lg:my-2">
       <div className="flex justify-between ml-6 mr-6 my-3">
         {menuOpen ? 
-          <button className="text-gray-400 hover:text-white focus:outline-none focus:text-white" onClick={toggleMenu}>
+          <button className="text-gray-400 hover:text-white focus:outline-none focus:text-white xl:hidden" onClick={toggleMenu}>
             <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button> :
-          <button className="text-gray-400 hover:text-white focus:outline-none focus:text-white" onClick={toggleMenu}>
+          <button className="text-gray-400 hover:text-white focus:outline-none focus:text-white xl:hidden" onClick={toggleMenu}>
             <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
@@ -40,14 +40,16 @@ const Navbar = ({ categories = [] }) => {
           <span className="snipcart-total-price font-semibold text-sm text-indigo-200"></span>
         </button>
       </div>
-      <div className={"flex flex-col justify-center md:flex-row mt-5 " + menuVisible}>
-        {categories.map((_category) => (
-          <Link href={`/categories/${_category.slug}`} key={_category.id}>
-            <a className="hover:text-white focus:bg-gray-900 focus:text-white text-gray-400 font-semibold py-2 px-4 mx-1" onClick={toggleMenu}>
-              {_category.name}
-            </a>
-          </Link>
-        ))}
+      <div className={menuVisible}>
+        <div className={"flex flex-col md:flex-row lg:flex-col justify-center mt-5"}>
+          {categories.map((_category) => (
+            <Link href={`/categories/${_category.slug}`} key={_category.id}>
+              <a className="hover:text-white focus:bg-gray-900 focus:text-white text-gray-400 font-semibold py-2 px-4 mx-1" onClick={toggleMenu}>
+                {_category.name}
+              </a>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
