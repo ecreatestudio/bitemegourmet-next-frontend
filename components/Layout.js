82118129ce512a7b78764cd421f6e409d8c41725
Scott Thomson <scott@ecreate.studio>
2021-02-03 22:51:42 +1100

    Set left nav to fill screen on desktop

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children, categories }) => {
   return (
     <div className="flex justify-center bg-gray-200">
       <div className="max-w-screen-xl flex flex-col min-h-screen w-full">
-        <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col lg:flex-row lg:h-full">
           <Navbar categories={categories} />
           <div className="lg:max-w-3/4 flex-grow">{children}</div>
         </div>
