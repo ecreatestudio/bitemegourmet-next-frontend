a54d9f393dca8433894fe3ac0c0c26ed38c3ff8c
Scott Thomson <scott@ecreate.studio>
2021-07-04 21:29:59 +1000

    Update to dark styling for category heading

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -14,9 +14,11 @@ const CategoryPage = ({ category }) => {
       <Head>
         <title>{category.name} gourmet food</title>
       </Head>
-      <h1 className="text-center md:text-left md:ml-5  mt-3 text-2xl font-sans font-semibold text-gray-800">
-        {category.name}
-      </h1>
+      <div className="ml-3 mr-3 mt-2 border rounded-lg bg-gray-100 bg-gray-900">
+        <h1 className="text-center md:text-left md:ml-5 mt-1 mb-1 text-2xl font-sans font-semibold text-gray-100">
+          {category.name}
+        </h1>
+      </div>
       <ProductsList products={category.products} />
     </div>
   );
