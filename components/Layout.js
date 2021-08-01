592496f5a076cc2aca8cbf554bd0d2745c01943e
Scott Thomson <scott@ecreate.studio>
2021-08-01 10:33:29 +1000

    Layout refactor & visual improvements

--- components/Layout.js
+++ components/Layout.js
@@ -6,14 +6,14 @@ const Layout = ({ children, categories }) => {
   return (
     <div className="flex justify-center bg-gray-200">
       <div className="max-w-screen-xl flex flex-col min-h-screen w-full">
-        <div className="flex flex-col h-12 text-center bg-red-500">
+        <div className="flex flex-col h-12 text-center bg-red-500 lg:rounded-b-lg">
           <h4 className="mt-3 font-semibold text-base leading-tight truncate text-white">
             OPENING SOON! NOT TAKING ORDERS YET.
           </h4>
         </div>
         <div className="flex flex-col lg:flex-row lg:h-full">
           <Navbar categories={categories} />
-          <div className="lg:h-full lg:max-w-3/4">{children}</div>
+          <div className="flex flex-col lg:max-w-3/4 lg:ml-2">{children}</div>
         </div>
         <Footer />
         <Snipcart />
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -20,85 +20,90 @@ const ProductPage = ({ product }) => {
   };
 
   return (
-    <div className="mt-3 mx-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
+    <div className="lg:h-full bg-gray-900 p-3 lg:rounded-lg my-2">
       <Head>
         <title>{product.title} gourmet food</title>
       </Head>
-      <div className="grid grid-cols-1 rounded-t-lg pb-2">
-        <h1 className="text-center md:text-left md:ml-1 text-2xl font-sans font-semibold block text-gray-800">
+      <div className="grid grid-cols-1">
+        <h1 className="text-center md:text-left md:ml-1 text-2xl font-sans font-semibold block text-white">
           {product.title}
         </h1>
-        <img
-          src={getStrapiMedia(product.image.formats.small.url)}
-          className="m-auto mt-2"
-          alt={product.title}
-        />
       </div>
-      <div className="w-full md:mt-12 flex flex-col justify-between">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
         <div>
-          <h4 className="mt-1 font-semibold text-lg leading-tight truncate text-gray-700">
-            Bite Me {product.title}
-          </h4>
-          <div className="mt-1 font-semibold text-lg">
-            ${product.price}{" "}
-            <span className="font-light">{product.serving}</span>
-          </div>
+          <img
+            src={getStrapiMedia(product.image.formats.small.url)}
+            className="m-auto mt-2"
+            alt={product.title}
+          />
+        </div>
+        <div className="w-full flex flex-col justify-between">
+          <div>
+            <h4 className="mt-1 font-semibold text-lg leading-tight truncate text-white">
+              Bite Me {product.title}
+            </h4>
+            <div className="mt-1 font-semibold text-lg text-white">
+              ${product.price}{" "}
+              <span className="font-light">{product.serving}</span>
+            </div>
 
-          {product.status === "published" ? (
-            <div className="flex flex-wrap">
-              <div className="flex-initial">
-                <div className="mt-4 text-gray-700 font-semibold pr-4">
-                  <label>Qty:</label>{" "}
-                  <input
-                    value={qtyValue}
-                    onChange={onQtyChange}
-                    id="quantity"
-                    type="number"
-                    inputMode="numeric"
-                    className="text-center py-2"
-                    style={{ maxWidth: 5 + "em" }}
-                  ></input>
+            {product.status === "published" ? (
+              <div className="flex flex-wrap">
+                <div className="flex-initial">
+                  <div className="mt-4 text-white font-semibold pr-4">
+                    <label>Qty:</label>{" "}
+                    <input
+                      value={qtyValue}
+                      onChange={onQtyChange}
+                      id="quantity"
+                      type="number"
+                      inputMode="numeric"
+                      className="text-gray-800 text-center py-2"
+                      style={{ maxWidth: 5 + "em" }}
+                    ></input>
+                  </div>
+                </div>
+                <div className="flex-auto">
+                  <button
+                    className="snipcart-add-item w-full mt-4 bg-green-300 border-2 d hover:shadow-lg text-white font-semibold 
+                  py-2 px-6 rounded shadow"
+                    data-item-id={product.id}
+                    data-item-price={product.price}
+                    data-item-url={router.asPath}
+                    data-item-description={product.description}
+                    data-item-image={getStrapiMedia(
+                      product.image.formats.thumbnail.url
+                    )}
+                    data-item-name={product.title}
+                    data-item-quantity={qtyValue}
+                    v-bind="customFields"
+                  >
+                    Add to cart
+                  </button>
                 </div>
               </div>
-              <div className="flex-auto">
-                <button
-                  className="snipcart-add-item w-full mt-4 bg-green-300 border-2 d hover:shadow-lg text-gray-700 font-semibold py-2 px-6 rounded shadow"
-                  data-item-id={product.id}
-                  data-item-price={product.price}
-                  data-item-url={router.asPath}
-                  data-item-description={product.description}
-                  data-item-image={getStrapiMedia(
-                    product.image.formats.thumbnail.url
-                  )}
-                  data-item-name={product.title}
-                  data-item-quantity={qtyValue}
-                  v-bind="customFields"
+            ) : (
+              <div className="text-center mr-10 mb-1" v-else>
+                <div
+                  className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+                  role="alert"
                 >
-                  Add to cart
-                </button>
+                  <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+                    Off the menu...
+                  </span>
+                  <span className="font-semibold mr-2 text-left flex-auto">
+                    This item is not currently available.
+                  </span>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="text-center mr-10 mb-1" v-else>
-              <div
-                className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
-                role="alert"
-              >
-                <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-                  Off the menu...
-                </span>
-                <span className="font-semibold mr-2 text-left flex-auto">
-                  This item is not currently available.
-                </span>
+            )}
+            <div className="mt-4 text-gray-200">{product.description}</div>
+            {product.ingredients !== null ? (
+              <div className="mt-4 text-gray-200">
+                <ReactMarkdown>{product.ingredients}</ReactMarkdown>
               </div>
-            </div>
-          )}
-          <div className="mt-4 text-gray-600">{product.description}</div>
-          {product.ingredients !== null ? (
-            <div className="mt-4 text-gray-600">
-              <ReactMarkdown>{product.ingredients}</ReactMarkdown>
-            </div>
-          ) : null}
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
